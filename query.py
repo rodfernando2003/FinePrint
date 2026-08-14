619beaa4c40ba4acb5e1bd9a61763593114ee7e2
rodfern2003 <131623523+rodfern2003@users.noreply.github.com>
2026-08-13 21:09:37 -0500

    Updated depreciated package

--- query.py
+++ query.py
@@ -9,7 +9,7 @@ import os
 # 5. StrOutputParser from langchain_core.output_parsers
 # 6. RunnablePassthrough from langchain_core.runnables
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableMap    
